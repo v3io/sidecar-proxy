a5bfdc401669c4bc807342aa68396cbf885f758e
Pavel <pavelr3@gmail.com>
2019-01-10 16:34:29 +0200

    Go fmt

--- main.go
+++ main.go
@@ -3,15 +3,17 @@ package main
 import (
 	"flag"
 	"os"
+
 	"github.com/v3io/proxy/app"
+
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
 
 	// args
-	listenAddress := flag.String("listen-addr", os.Getenv("PROXY_LISTEN_ADDRESS"),"Port to listen on")
-	forwardAddress := flag.String("forward-addr", os.Getenv("PROXY_FORWARD_ADDRESS"),"IP /w port to forward to")
+	listenAddress := flag.String("listen-addr", os.Getenv("PROXY_LISTEN_ADDRESS"), "Port to listen on")
+	forwardAddress := flag.String("forward-addr", os.Getenv("PROXY_FORWARD_ADDRESS"), "IP /w port to forward to")
 	flag.Parse()
 
 	// logger conf
