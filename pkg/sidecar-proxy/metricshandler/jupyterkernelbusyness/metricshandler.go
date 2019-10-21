24a5c2327645138cb22346ad6e0ab6e163fefa9a
Hedingber <hedingber@gmail.com>
2019-10-22 02:04:57 +0300

    more readable code

--- pkg/sidecar-proxy/metricshandler/jupyterkernelbusyness/metricshandler.go
+++ pkg/sidecar-proxy/metricshandler/jupyterkernelbusyness/metricshandler.go
@@ -25,6 +25,10 @@ type jupyterKernelBusynessMetricsHandler struct {
 	metric         *prometheus.GaugeVec
 }
 
+type kernel struct {
+	executionState metricshandler.KernelExecutionState
+}
+
 func NewJupyterKernelBusynessMetricsHandler(logger *logrus.Logger,
 	forwardAddress string,
 	listenAddress string,
@@ -63,52 +67,22 @@ func (n *jupyterKernelBusynessMetricsHandler) Start() {
 	errc := make(chan error)
 	go func() {
 		for range ticker.C {
-			var kernelsList []interface{}
-			kernelsEndpoint := fmt.Sprintf("http://%s/api/kernels", n.forwardAddress)
-			resp, err := http.Get(kernelsEndpoint)
+			kernels, err := n.getKernels()
 			if err != nil {
-				errc <- errors.Wrapf(err, "Failed to send request to kernels endpoint: %s", kernelsEndpoint)
-			}
-			body, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				errc <- errors.Wrapf(err, "Failed to read response body: %s", resp.Body)
+				errc <- errors.Wrap(err, "Failed to get kernels")
 			}
 
-			if err := json.Unmarshal(body, &kernelsList); err != nil {
-				errc <- errors.Wrapf(err, "Failed to unmarshal response body: %s", body)
-			}
-
-			foundBusyKernel := false
-			for _, kernelStr := range kernelsList {
-				kernelMap, ok := kernelStr.(map[string]interface{})
-				if !ok {
-					errc <- errors.Errorf("Could not parse kernel string: %s", kernelStr)
+			isBusyKernelExists := n.isBusyKernelExists(kernels)
+			if isBusyKernelExists {
+				if err := n.setMetric(metricshandler.BusyKernelExecutionState); err != nil {
+					errc <- errors.Wrapf(err, "Failed to set metric")
 				}
+			} else {
 
-				kernelExecutionState, ok := kernelMap["execution_state"].(string)
-				if !ok {
-					errc <- errors.Errorf("Could not parse kernel execution state: %s", kernelMap["execution_state"])
-				}
-
-				// If one of the kernels is busy - it's busy
-				if kernelExecutionState == string(metricshandler.BusyKernelExecutionState) {
-					if err := n.setMetric(metricshandler.BusyKernelExecutionState); err != nil {
-						errc <- errors.Wrapf(err, "Failed to set metric")
-					}
-					foundBusyKernel = true
-					break
-				}
-			}
-
-			// If non of the kernels is busy - it's idle
-			if !foundBusyKernel {
+				// If none of the kernels is busy - it's idle
 				if err := n.setMetric(metricshandler.IdleKernelExecutionState); err != nil {
 					errc <- errors.Wrapf(err, "Failed to set metric")
 				}
-			}
-
-			if err := resp.Body.Close(); err != nil {
-				errc <- errors.Wrap(err, "Failed closing response body")
 			}
 		}
 	}()
@@ -118,6 +92,56 @@ func (n *jupyterKernelBusynessMetricsHandler) Start() {
 			n.logger.WithError(err).Warn("Failed setting metric")
 		}
 	}
+}
+
+func (n *jupyterKernelBusynessMetricsHandler) getKernels() ([]kernel, error) {
+	var parsedKernelsList []kernel
+	var kernelsList []interface{}
+	kernelsEndpoint := fmt.Sprintf("http://%s/api/kernels", n.forwardAddress)
+	resp, err := http.Get(kernelsEndpoint)
+	if err != nil {
+		return []kernel{}, errors.Wrapf(err, "Failed to send request to kernels endpoint: %s", kernelsEndpoint)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return []kernel{}, errors.Wrapf(err, "Failed to read response body: %s", resp.Body)
+	}
+
+	if err := json.Unmarshal(body, &kernelsList); err != nil {
+		return []kernel{}, errors.Wrapf(err, "Failed to unmarshal response body: %s", body)
+	}
+
+	for _, kernelStr := range kernelsList {
+		kernelMap, ok := kernelStr.(map[string]interface{})
+		if !ok {
+			return []kernel{}, errors.Errorf("Could not parse kernel string: %s", kernelStr)
+		}
+
+		kernelExecutionStateStr, ok := kernelMap["execution_state"].(string)
+		if !ok {
+			return []kernel{}, errors.Errorf("Could not parse kernel execution state: %s", kernelMap["execution_state"])
+		}
+
+		kernelExecutionState, err := metricshandler.ParseKernelExecutionState(kernelExecutionStateStr)
+		if err != nil {
+			return []kernel{}, errors.Wrapf(err, "Failed to parse kernel execution state: %s", kernelExecutionStateStr)
+		}
+		parsedKernelsList = append(parsedKernelsList, kernel{executionState: kernelExecutionState})
+	}
+
+	if err := resp.Body.Close(); err != nil {
+		return []kernel{}, errors.Wrap(err, "Failed closing response body")
+	}
+	return parsedKernelsList, nil
+}
+
+func (n *jupyterKernelBusynessMetricsHandler) isBusyKernelExists(kernels []kernel) bool {
+	for _, kernel := range kernels {
+		if kernel.executionState == metricshandler.BusyKernelExecutionState {
+			return true
+		}
+	}
+	return false
 }
 
 func (n *jupyterKernelBusynessMetricsHandler) setMetric(kernelExecutionState metricshandler.KernelExecutionState) error {
--- pkg/sidecar-proxy/metricshandler/metricshandler.go
+++ pkg/sidecar-proxy/metricshandler/metricshandler.go
@@ -1,5 +1,9 @@
 package metricshandler
 
+import (
+	"github.com/nuclio/errors"
+)
+
 type MetricHandler interface {
 	RegisterMetrics() error
 	Start()
@@ -18,3 +22,14 @@ const (
 	IdleKernelExecutionState KernelExecutionState = "idle"
 	BusyKernelExecutionState KernelExecutionState = "busy"
 )
+
+func ParseKernelExecutionState(kernelExecutionStateStr string) (KernelExecutionState, error) {
+	switch kernelExecutionStateStr {
+	case string(BusyKernelExecutionState):
+		return BusyKernelExecutionState, nil
+	case string(IdleKernelExecutionState):
+		return IdleKernelExecutionState, nil
+	default:
+		return "", errors.Errorf("Unknown kernel execution state: %s", kernelExecutionStateStr)
+	}
+}
