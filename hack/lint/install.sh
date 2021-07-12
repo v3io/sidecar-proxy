7e76551a2f40d2f93db0b0f4514119ef5d688fc9
omesser <odedmesser@gmail.com>
2021-07-12 20:31:34 +0300

    install.sh changes

--- hack/lint/install.sh
+++ hack/lint/install.sh
@@ -4,13 +4,13 @@ set -e
 echo Installing linters...
 
 OS_NAME=$(uname)
-FORCE_INSTALL=false
 BIN_DIR=$(pwd)/.bin/
 
 echo "Creating bin directory: ${BIN_DIR}"
 mkdir -p ${BIN_DIR}
 
-if [[ ! -f ${BIN_DIR}/impi ]] ; then
+if [[ ! -f ${BIN_DIR}/impi ]]
+  then
     echo "impi binary does not exist. Fetching and installing..."
     curl -sSfL https://api.github.com/repos/pavius/impi/releases/latest \
       | grep -i "browser_download_url.*impi.*${OS_NAME}" \
@@ -22,13 +22,14 @@ if [[ ! -f ${BIN_DIR}/impi ]] ; then
     echo "impi installed in: ${BIN_DIR}/impi"
 fi
 
-if [[ $# -ne 0 && "$1" == "force" ]]
+if [[ ( "${FORCE_INSTALL}" == "true" ) || ( $# -ne 0 && "$1" == "force" ) ]]
   then
     echo "Force install golangci-lint requested"
     FORCE_INSTALL=true
 fi
 
-if [[ $FORCE_INSTALL = true || ! -f ${BIN_DIR}/golangci-lint ]] ; then
+if [[ "${FORCE_INSTALL}" == "true" || ! -f ${BIN_DIR}/golangci-lint ]]
+  then
     echo "golangci-lint binary does not exist or force install requested. Fetching and installing..."
 
     # upgrade from v1.24.0 when https://github.com/golangci/golangci-lint/issues/974 is resolved
